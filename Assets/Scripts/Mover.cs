ec2dfe96e59d79dd7c42222f90892e4bc5929733
GivenFlesh <64552095+GivenFlesh@users.noreply.github.com>
2022-01-04 14:33:38 -0800

    Slowed "Z" movement
    mover script now has new serialized field "zOffsetRatio" for determining how much to slow the player in the fake z axis

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 25;
+    [Range(1,0)] [SerializeField] float zOffsetRatio = 0.5f;
 
     SpriteRenderer _renderer;
     Rigidbody2D _rigidBody;
@@ -16,14 +17,12 @@ public class Mover : MonoBehaviour
         _rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    void Start()
-    {
-
-    }
-
     public void Move(Vector2 direction)
     {
-        Vector2 delta = direction * moveSpeed * (Time.fixedDeltaTime * 10f);
+
+        Vector2 delta = direction;
+        delta.y *= zOffsetRatio;
+        delta *= moveSpeed * (Time.fixedDeltaTime * 10f);
         _rigidBody.velocity = delta;
         if( Mathf.Sign(_rigidBody.velocity.x) != transform.localScale.x && _rigidBody.velocity.x != 0)
         {
